da8dc6a690a3cdd458ff4e613c96e368922b4b84
Armine Azizyan <armineazizyann@gmail.com>
2023-12-01 14:45:43 +0400

    updated test name

---
+++ cypress/e2e/inter_req.cy.ts
@@ -0,0 +1,37 @@
+describe("Request/Intercept",()=>{
+  //beforeEach(()=>{
+
+  //   cy.visit("/");
+  
+ // })
+
+
+  it("should contain title text using request", ()=>{
+
+     cy.request("/").its("body").should('include', "<h1>Lorem Ipsum</h1>")
+        
+     })
+
+
+it("should modify response using intercept",()=>{
+
+    cy.intercept({
+      url: 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json',  
+     },(req) => {
+
+        req.reply((res) =>
+        {
+          res.send({ fixture: 'data.json' });
+        })
+
+      }).as("data")
+  
+  cy.visit("https://mdn.github.io/learning-area/javascript/oojs/json/heroes-finished.html")
+
+  cy.wait("@data").its('response')
+  .should(response => expect(response.statusCode).to.eq(200))   
+  .should(response => expect(response.body.members.length).to.eq(1))
+  .should(response => expect(response.body.members[0].name).to.contain("Sherlock Holmes"))
+  
+})
+})
